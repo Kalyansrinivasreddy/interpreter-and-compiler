df9a88f5829d540d448683bd0b62cf39f6d4e231
M.Kalyan Srinivas Reddy <kalyansrinivas.mule@gmail.com>
2018-06-07 23:21:39 +0530

    Added required solutions for the given problems.

---
+++ interpreterAndCompiler.py
@@ -0,0 +1,17 @@
+Interpreter:
+      An interpreter is a computer program that executes source code or byte code.
+Examples:
+      ruby(interpreter)
+      python(interpreter)
+      php(interpreter)
+
+
+
+
+Compiler:
+     Compiler is used to covert high level languages into machine code language.
+Examples:
+     gcc
+     javac
+     .pyc
+     
